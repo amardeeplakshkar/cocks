1e552d0e17366da933334771d64ffb33285cf35b
amardeeplakshkar <109571797+amardeeplakshkar@users.noreply.github.com>
2024-09-26 05:58:49 +0530

    Update page.tsx

--- app/(routes)/leaderboard/page.tsx
+++ app/(routes)/leaderboard/page.tsx
@@ -15,22 +15,6 @@ interface UserData {
 const Leaderboard = () => {
   const [userData, setUserData] = useState<UserData | null>(null);
 
-  // Updated colors to use 700 value
-  const brightColors = [
-    "bg-yellow-700", 
-    "bg-green-700", 
-    "bg-pink-700", 
-    "bg-blue-700", 
-    "bg-red-700", 
-    "bg-purple-700", 
-    "bg-teal-700", 
-    "bg-orange-700"
-  ];
-
-  const getRandomColor = () => {
-    return brightColors[Math.floor(Math.random() * brightColors.length)];
-  };
-
   useEffect(() => {
     if (typeof window !== "undefined" && WebApp.initDataUnsafe.user) {
       setUserData(WebApp.initDataUnsafe.user as UserData);
@@ -52,16 +36,16 @@ const Leaderboard = () => {
         <h2 className="text-2xl font-bold py-2">Wall Of Frame</h2>
         <div className="w-full py-4">
           <Card
-            bg={getRandomColor()}
+            bg={"bg-white/10"}
             rank="90"
             amount={150}
             username={userData.username || 'N/A'}
           />
         </div>
         <h3 className="place-self-start font-bold text-xl">Top Users</h3>
-        <Card rank="#1" amount={160} username="Amardeep" bg={getRandomColor()} />
-        <Card rank="#2" amount={140} username="Priya" bg={getRandomColor()} />
-        <Card rank="#3" amount={50} username="Shravan" bg={getRandomColor()} />
+        <Card rank="#1" amount={160} username="Amardeep" />
+        <Card rank="#2" amount={140} username="Priya" />
+        <Card rank="#3" amount={50} username="Shravan" />
       </main>
     </>
   );
